53aa8770f5e2c9f869b399ad3926980525a552d9
Tanishq Baweja <tanishq.baweja001@gmail.com>
2022-01-28 20:42:34 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Responsive Personal Website(DSC TASK) 
---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+// MENU SHOW
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+// REMOVE MENU
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+// SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY >= sectionTop && scrollY < sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+
+    if(scrollY > 1700){
+        document.querySelector('.nav__menu a[href*=' + 'skills' + ']').classList.remove('active')
+        document.querySelector('.nav__menu a[href*=' + 'hobbies' + ']').classList.add('active')
+    }
+}
+
+
+
